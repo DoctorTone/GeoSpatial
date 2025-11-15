05e447dd1ab9511da3bc6d0d737e471c4998751d
DoctorTone <tony@drt-software.com>
2025-11-15 18:40:22 +0000

    Changed title colour and position

--- src/UI/Title.tsx
+++ src/UI/Title.tsx
@@ -1,9 +1,14 @@
 import Typography from "@mui/material/Typography";
+import BoltIcon from "@mui/icons-material/Bolt";
 
 const Title = () => {
   return (
     <div id="title" className="panel">
-      <Typography variant="h3">Lightning</Typography>
+      <Typography variant="h3" sx={{ color: "orange" }}>
+        <BoltIcon fontSize="large" />
+        Lightning
+        <BoltIcon fontSize="large" />
+      </Typography>
     </div>
   );
 };
